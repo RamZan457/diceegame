762e6d913511957f0e391de949280145f4593c3d
Mohammad Ramzan <114152849+RamZan457@users.noreply.github.com>
2023-07-16 14:22:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// For Dice Image 1
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var image1 = document.querySelector(".img1");
+// image1.setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+image1.setAttribute("src", `./images/dice${randomNumber1}.png`);
+
+// For Dice Image 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+var image2 = document.querySelector(".img2");
+image2.setAttribute("src", `./images/dice${randomNumber2}.png`);
+
+// For changing Text to Winner or Draw
+if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerHTML = "Draw!";
+} else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "&#128681; Player 1 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = " &#128681; Player 2 Wins!";
+}
